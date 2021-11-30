c1c68e80a04c04a28617423ececa25a8a9b804d9
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-11-30 10:02:45 +0300

    Add files via upload

---
+++ Zadanie 1.1 Form.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zadanie_1._1_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double s, r;
+            s = Convert.ToDouble(textBox1.Text);
+            r = Math.Sqrt(s / 6);
+            textBox2.Text = Convert.ToString(r);
+        }
+    }
+}
---
+++ Zadanie 1.2 Form.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zadanie_1_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num; 
+            num  = Convert.ToDouble(textBox1.Text);
+            double sum = num % 10 + (num / 10) % 10;
+
+            textBox2.Text = Convert.ToString((sum % 3) == 0 ? "Кратно" : "Не кратно");
+        }
+    }
+}
