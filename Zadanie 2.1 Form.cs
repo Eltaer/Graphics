115566ed00a0fbe5d3611d7401526a6e2e57964d
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-01 09:10:05 +0300

    Add files via upload

---
+++ Zadanie 2.1 Form.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zadanie_2._1_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            x = Convert.ToDouble(textBox1.Text);
+            y = Convert.ToDouble(textBox2.Text);
+
+            if ((x * x + y * y < 144) && y - Math.Abs(x) > 0)
+            {
+                textBox3.Text = Convert.ToString("Точка попала в заштрихованную область");
+            }
+            else if ((x * x + y * y == 144) && y - Math.Abs(x) == 0)
+
+            {
+                textBox3.Text = Convert.ToString("Точка на границе заштрихованной области");
+            }
+            else
+            {
+                textBox3.Text = Convert.ToString("Точка не попала в заштрихованную область");
+            }
+        }
+    }
+}
---
+++ Zadanie 2.2 Form.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zadanie_2._2_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int m;
+            m = Convert.ToInt32(textBox1.Text);
+            string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+            textBox2.Text = Convert.ToString(months[DateTime.Now.AddMonths(-m).Month-1]);
+        }
+    }
+}
