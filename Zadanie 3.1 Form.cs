d99e22c186b88de796f04f2f49b6be9df888fb6b
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-05 14:19:30 +0300

    Add files via upload

---
+++ Zadanie 3.1 Form.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zadanie_3._1_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double f(double x)
+        {
+            return x % 10;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                double a = Convert.ToDouble(textBox1.Text);
+
+                double b = Convert.ToDouble(textBox2.Text);
+                double z = f(a) + f(b);
+                textBox3.Text = Convert.ToString(z);
+        }
+    }
+}
