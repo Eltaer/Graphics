4fe3fe4d5957af152dda5ccc9eb5ba29cc425768
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-16 09:48:24 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int[] n = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            string FileName = @"C:\Users\stu-pkspk219\Desktop\file.txt";
+            using (StreamWriter sw = new StreamWriter(FileName))
+            {
+                foreach (int i in n)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sw.WriteLine(i);
+                    }
+                }
+            }
+
+            using (StreamReader sr = new StreamReader(FileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    textBox1.Text +=(line) + "\r\n";
+                }
+            }
+        }
+    }
+}
