3e3976187fb0f5037e57869bda5be8d92a9ae0c2
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-20 11:36:09 +0300

    Update Zadanie 11 Form.cs

--- Zadanie 11 Form.cs
+++ Zadanie 11 Form.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Zadanie_11_Form
+namespace _11_12
 {
     public partial class Form1 : Form
     {
@@ -16,6 +16,7 @@ namespace Zadanie_11_Form
         {
             InitializeComponent();
         }
+
         class Date
         {
             DateTime date;
@@ -56,36 +57,69 @@ namespace Zadanie_11_Form
                 get { return DateTime.IsLeapYear(date.Year); }
             }
 
+            public DateTime this[int index]
+            {
+                get { return date.AddDays(index); }
+            }
+
+            public static bool operator !(Date a)
+            {
+                return DateTime.DaysInMonth(a.Date1.Year, a.Date1.Month) != a.Date1.Day;
+            }
+
+            public static bool operator true(Date a)
+            {
+                return a.date.Day == 1 && a.date.Month == 1;
+            }
+
+            public static bool operator false(Date a)
+            {
+                return a.date.Day != 1 || a.date.Month != 1;
+            }
+
+            public static bool operator &(Date a, Date b)
+            {
+                return a.Date1.Equals(b.Date1);
+            }
+
+            public static explicit operator String(Date obj)
+            {
+                return " Год: " + obj.date.Year + " Месяц: " + obj.date.Month + " Число: " + obj.date.Day;
+            }
         }
         private void button1_Click(object sender, EventArgs e)
-        {
-            string st1 = "2009,1,1";
-            Date f = new Date(st1);
-            textBox1.Text += ("Определенная дата: ");
-            textBox1.Text += Convert.ToString(f.Date1 + "\r\n");
-            textBox1.Text += ("Прошлый день: ");
-            textBox1.Text += Convert.ToString(f.PrevDay() + "\r\n");
-            textBox1.Text += ("Следующий день: ");
-            textBox1.Text += Convert.ToString(f.NextDay() + "\r\n");
-            textBox1.Text += ("Дней до конца месяца: ");
-            textBox1.Text += Convert.ToString(f.DaysLeft() + "\r\n");
-            textBox1.Text += ("Високосный ли год?: ");
-            textBox1.Text += Convert.ToString(f.IsLeap);
+            {
+                string st1 = "2009,1,1";
+                Date f = new Date(st1);
+                textBox4.Text += ("Определенная дата: ");
+                textBox4.Text += Convert.ToString(f.Date1 + "\r\n");
+                textBox4.Text += ("Прошлый день: ");
+                textBox4.Text += Convert.ToString(f.PrevDay() + "\r\n");
+                textBox4.Text += ("Следующий день: ");
+                textBox4.Text += Convert.ToString(f.NextDay() + "\r\n");
+                textBox4.Text += ("Дней до конца месяца: ");
+                textBox4.Text += Convert.ToString(f.DaysLeft() + "\r\n");
+                textBox4.Text += ("Високосный ли год?: ");
+                textBox4.Text += Convert.ToString(f.IsLeap);
 
-            Console.Write("Введите дату: ");
-            string st = Convert.ToString(textBox3.Text);
-            Date A = new Date(st);
+                Console.Write("введите i: ");
+                string st2 = Convert.ToString(textBox1.Text);
+                int i = Convert.ToInt16(st2);
 
-            textBox2.Text += ("Заданная дата: ");
-            textBox2.Text += (A.Date1 + "\r\n");
-            textBox2.Text += ("Прошлый день: ");
-            textBox2.Text += (A.PrevDay() + "\r\n");
-            textBox2.Text += ("Следующий день: ");
-            textBox2.Text += (A.NextDay() + "\r\n");
-            textBox2.Text += ("Дней до конца месяца: ");
-            textBox2.Text += (A.DaysLeft() + "\r\n");
-            textBox2.Text += ("Високосный ли год?: ");
-            textBox2.Text += (A.IsLeap);
+                Console.Write("Введите дату: ");
+                string st = Convert.ToString(textBox2.Text);
+                Date A = new Date(st);
+
+                textBox3.Text += ("Заданная дата: ");
+                textBox3.Text += (A.Date1 + "\r\n");
+                textBox3.Text += ("Прошлый день: ");
+                textBox3.Text += (A.PrevDay() + "\r\n");
+                textBox3.Text += ("Следующий день: ");
+                textBox3.Text += (A.NextDay() + "\r\n");
+                textBox3.Text += ("Дней до конца месяца: ");
+                textBox3.Text += (A.DaysLeft() + "\r\n");
+                textBox3.Text += ("Високосный ли год?: ");
+                textBox3.Text += (A.IsLeap);
+            }
         }
     }
-}
